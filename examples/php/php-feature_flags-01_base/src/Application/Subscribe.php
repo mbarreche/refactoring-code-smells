1f8c118233aef705ace22b53afe3f1072e45f1ce
Marcos Antonio Barreche Salguero <mbarreche@freepik.com>
2023-05-01 19:24:41 +0200

    refactor: delete single pattern in class Subscribe

--- examples/php/php-feature_flags-01_base/src/Application/Subscribe.php
+++ examples/php/php-feature_flags-01_base/src/Application/Subscribe.php
@@ -12,21 +12,21 @@ use CodelyTv\Persistence\MySqlConnection;
 
 final class Subscribe
 {
+    public function __construct(private MySqlConnection $connection, private FeatureFlags $featureFlags, private EmailNotifier $emailNotifier) {
+
+    }
+
     public function __invoke(string $email, ?string $name = null): void
     {
-        if (Debug::instance()->isDebugModeEnabled()) {
-            FeatureFlags::instance()->deactivateAll();
-        }
-
-        $flag = FeatureFlags::instance()->get(Flags::NEW_SUBSCRIPTION_PAGE_NAME);
+        $flag = $this->featureFlags->get(Flags::NEW_SUBSCRIPTION_PAGE_NAME);
 
         if ($flag) {
             // The new subscription added a "name" field
-            MySqlConnection::instance()->persist($email, $name);
+            $this->connection->persist($email, $name);
         } else {
-            MySqlConnection::instance()->persist($email);
+            $this->connection->persist($email);
         }
 
-        EmailNotifier::instance()->sendSubscriptionEmail($email);
+        $this->emailNotifier->sendSubscriptionEmail($email);
     }
 }
--- examples/php/php-feature_flags-01_base/src/SubscribeController.php
+++ examples/php/php-feature_flags-01_base/src/SubscribeController.php
@@ -5,22 +5,36 @@ declare(strict_types=1);
 namespace CodelyTv;
 
 use CodelyTv\Application\Subscribe;
+use CodelyTv\Email\EmailNotifier;
+use CodelyTv\Persistence\MySqlConnection;
 use Symfony\Component\HttpFoundation\Request;
 
 final class SubscribeController
 {
     public function __invoke(Request $request)
     {
-        $flagHeader = $request->headers->get('X-FLAG');
+        $subscribeUseCase = new Subscribe(
+            MySqlConnection::instance(),
+            $this->getFeatures($request),
+            EmailNotifier::instance()
+        );
 
-        if ($flagHeader === Flags::NEW_SUBSCRIPTION_PAGE_TOKEN) {
-            FeatureFlags::instance()->set('new_subscription_page', true);
-        }
-
-        $subscribeUseCase = new Subscribe();
         $subscribeUseCase->__invoke(
             $request->request->get('email'),
             $request->request->get('name'),
         );
+    }
+
+    private function getFeatures(Request $request): FeatureFlags
+    {
+        $flagHeader = $request->headers->get('X-FLAG');
+        $features = FeatureFlags::instance();
+        if ($flagHeader === Flags::NEW_SUBSCRIPTION_PAGE_TOKEN) {
+            $features->set('new_subscription_page', true);
+        }
+        if (Debug::instance()->isDebugModeEnabled()) {
+            $features->deactivateAll();
+        }
+        return $features;
     }
 }
